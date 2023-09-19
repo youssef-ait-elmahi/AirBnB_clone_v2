c838479d33049db78d45e2a990530980ec4b3c8c
Ahmedkel <ahmedkeloch@gmail.com>
2023-09-19 21:10:55 +0100

    Fixing : missing description for sql setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+-- Create a database and a user for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
