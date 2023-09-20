2447673e7feaffd597c93f5a5364b3e94dc49b57
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-09-20 22:31:00 +0100

    checkif task 5 working

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-
-import os
 from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 
-# Determine the storage type based on the environment variable
-storage_type = os.environ.get('HBNB_TYPE_STORAGE', 'file')
 
-if storage_type == 'db':
-    storage = DBStorage()
-else:
-    storage = FileStorage()
-
-# Load data from the storage system
+storage = FileStorage()
 storage.reload()
--- models/place.py
+++ models/place.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
 class Place(BaseModel):
