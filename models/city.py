e0810cb72c71c4f8c937c2864f2d6c2515004baa
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-09-20 20:29:10 +0100

    city place user (edited 2) task 8

--- models/city.py
+++ models/city.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
 
 
 class City(BaseModel, Base):
-    """City class"""
+    """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-
-    # Add the relationship with Place
-    places = relationship(
-        "Place", cascade="all, delete", back_populates="cities")
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
--- models/user.py
+++ models/user.py
@@ -2,7 +2,6 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -13,7 +12,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
-    # Add the relationship with Place
-    places = relationship(
-        "Place", cascade="all, delete", back_populates="user")
