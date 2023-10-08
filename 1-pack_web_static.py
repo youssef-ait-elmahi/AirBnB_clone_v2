344e35d4d4dcf9470b94f025114b4f4147622d5e
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-10-08 16:29:52 +0100

    Deploy static task 2 edit

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-# Fabfile to generates a .tgz archive from the contents of web_static.
-import os.path
+"""Fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo, using the function do_pack."""
+from fabric.api import local, task
 from datetime import datetime
-from fabric.api import local
+import os
 
 
+@task
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
-        return None
-    return file
+    """Function that archive web_static folder """
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    mkdir = "mkdir -p versions"
+    path = "versions/web_static_{}.tgz".format(date)
+    print("Packing web_static to {}".format(path))
+    if local('{} && tar -cvzf {} web_static'.format(mkdir, path)).succeeded:
+        size = os.path.getsize(path)
+        print("web_static packed: {} -> {}Bytes".format(path, size))
+        return path
+    return None
