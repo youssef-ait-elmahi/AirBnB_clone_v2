93b685a10250e28b9371fc5ba9ffe4135567a8ad
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-10-21 15:26:58 +0100

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """ Method that returns Hello HBNB! """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Method that returns Hello HBNB! """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    This function takes a string argument 'text',
+    replaces all underscores with spaces,
+    and returns a string in the format 'C <text>'
+    when the /c/<text> route is accessed.
+    Args:
+        text (str): The text to be displayed after 'C '
+    Returns:
+        str: A string in the format 'C <text>'
+    """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5000)
