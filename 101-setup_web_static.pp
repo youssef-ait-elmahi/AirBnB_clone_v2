7eda61a02f2e4caa63cdeb92cf36ca86a1c191bf
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-10-21 02:07:12 +0100

    task 101

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,45 @@
+file { '/data':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/releases':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/shared':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => '<html>
+    <head>
+    </head>
+    <body>
+      Holberton School
+    </body>
+</html>',
+}
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test',
+}
