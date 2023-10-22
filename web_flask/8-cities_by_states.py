cb4eafe52e87c082adf34dcdeee2dd558246d776
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-10-22 21:08:49 +0100

    tasks 8 and 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage, State
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
